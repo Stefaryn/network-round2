44bafc0036aa24b36f8c20b4ed7398e57c8f251d
Dingus <devindperez@gmail.com>
2019-06-18 13:57:50 -0700

    minor changes

--- head.cc
+++ head.cc
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]){
   int rows = 5;
   int cols = 5;
   float **ptr = nullptr;
-  Matrix::mat_malloc(ptr, rows, cols);
+  //Matrix::mat_malloc(ptr, rows, cols);
   //Matrix m(rows, cols);
   //Matrix m2(ptr, rows, cols);
-  Matrix::mat_free(ptr, rows);
+  //Matrix::mat_free(ptr, rows);
 }
